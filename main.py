331d1e685fe602c8b35fe9ae7b26d819a2d23823
rpfarish <60075390+rpfarish@users.noreply.github.com>
2020-03-12 00:13:32 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import random
 
+# TODO Add move blocking for AI
 
-# Task list
-
-# This is only a user vs rand comp
-# TODO make different modes
 
 # Start game
 def start(player_1=0, player_2=1):
@@ -65,10 +62,6 @@ def start(player_1=0, player_2=1):
         def clear_board(self):
             self.board = [None] * 9
 
-        def loop_input(self):
-            # TODO loop input
-            pass
-
         def is_valid(self, index):
             """:returns bool"""
 
@@ -76,9 +69,17 @@ def start(player_1=0, player_2=1):
                 self.num_list[index] = index
                 return True
             elif index in self.num_list:
-                # TODO don't print when its comp turn and dont print board
+                # TODO don't print when its comp turn and don't print board(make it stop)
                 print(f"hey, {index + 1} is not a valid input ")
-                self.loop_input()
+
+        def comp_is_valid(self):
+            """:returns list of open/valid moves"""
+            valid_li = []
+            for i in range(len(self.num_list)):
+                if self.num_list[i] == 10:
+                    valid_li.append(i)
+
+            return valid_li
 
         def write_board(self, index):
             not_used = self.is_valid(index)
@@ -122,9 +123,35 @@ def start(player_1=0, player_2=1):
     b2 = Hashtag()
     b2.example_board()
 
+    # make available list
+    def best_move(avail, board, who):
+        if who == 'O':
+            for i in avail:
+                new_board = board[:]
+                new_board[i] = 'O'
+                good = has_won(new_board)
+                # do the move
+                if good:
+                    return str(i + 1)
+            else:
+                return -1
+        elif who == 'X':
+            for i in avail:
+                new_board = board[:]
+                new_board[i] = 'X'
+                good = has_won(new_board)
+                # do the move
+                if good:
+                    return str(i + 1)
+            else:
+                return -1
+    #         The stop move func
+
     # b2.display_board()
 
-
+    # Available spaces list
+    # ip all moves from list and check if each produces a win
+    # and if true return move
 
     # Main loop and check win
     while not has_won(b1.board) and 10 in b1.num_list:
@@ -142,6 +169,14 @@ def start(player_1=0, player_2=1):
             elif player_1 == 1:
                 var1 = random.randint(0, 9)
                 var1 = str(var1)
+            elif player_1 == 3:
+                hi = b1.comp_is_valid()
+                new_board = b1.board[:]
+                gold = best_move(hi, new_board, 'X')
+                if gold != -1:
+                    var1 = gold
+                else:
+                    var1 = str(random.randint(0, 9))
             else:
                 var1 = '0'
             # checks if the move if valid and if not loop input until valid
@@ -159,6 +194,7 @@ def start(player_1=0, player_2=1):
                 b1.display_board()
         # Player 2
         elif not b1.is_x_turn:
+
             if player_2 == 0:
                 # Prompts Player 2 for their move
                 var1 = input(f"It's {whosturn}'s turn. \nEnter your move here: ")
@@ -166,7 +202,13 @@ def start(player_1=0, player_2=1):
                 # Computer plays random move
                 var1 = str(random.randint(0, 9))
             elif player_2 == 3:
-                var1 = best_move()
+                hi = b1.comp_is_valid()
+                new_board = b1.board[:]
+                gold = best_move(hi, new_board, 'O')
+                if gold != -1:
+                    var1 = gold
+                else:
+                    var1 = str(random.randint(0, 9))
             else:
                 var1 = '0'
 
@@ -178,6 +220,7 @@ def start(player_1=0, player_2=1):
                     var1 = str(random.randint(0, 9))
                 elif player_2 == 2:
                     var1 = best_move()
+            #         Repeat code?
             var1 = int(var1)
             if 1 <= var1 <= 9:
                 var1 -= 1
@@ -198,26 +241,44 @@ def start(player_1=0, player_2=1):
 
 # TODO choose which player goes first and what piece they are
 def init_func_game(other=False):
+    def ip_func(init_the_game):
+        if init_the_game == 'n' or init_the_game == 'no' or init_the_game == 'quit' or init_the_game == 'nope':
+            quit()
+        mode = input("Enter Option Number 1-9:\n    1:Human vs Computer AI\n    2:Human vs Computer Random.\n"
+                     "    3:Human vs Human\n    4:Computer AI vs Human\n    5:Computer Random vs Human\n"
+                     "    6:Computer Random vs Computer AI\n    7:Computer AI vs Computer Random\n"
+                     "    8:Computer AI vs Computer AI\n    9:Computer Random vs Computer Random\n>")
+        # 0:Human 1:Comp Rand 3:Comp AI
+        if mode == '1':
+            start(0, 3)
+        elif mode == '2':
+            start(0, 1)
+        elif mode == '3':
+            start(0, 0)
+        elif mode == '4':
+            start(3, 0)
+        elif mode == '5':
+            start(1, 0)
+        elif mode == '6':
+            start(1, 3)
+        elif mode == '7':
+            start(3, 1)
+        elif mode == '8':
+            start(3, 3)
+        elif mode == '9':
+            start(1, 1)
+
     if not other:
         # ask user if they want to play again: later
         init_game = input('Do you want to play a game?\n> ')
         init_game = init_game.lower()
-        mode = input("Enter 1 or 2\n1: Human vs Human\n2: Human vs Computer Random.\n>")
         # TODO filter input
-        if init_game == 'n' or init_game == 'no' or init_game == 'quit':
-            quit()
-        elif mode == '1':
-            start(0, 0)
-        elif mode == '2':
-            start(0, 1)
+        ip_func(init_game)
     if other:
         # ask user if they want to play again: later
         init_game = input('Do you want to play again?\n>')
         init_game = init_game.lower()
-        if init_game == 'n' or init_game == 'no' or init_game == 'quit':
-            quit()
-        else:
-            start(0, 1)
+        ip_func(init_game)
 
 
 init_func_game()
